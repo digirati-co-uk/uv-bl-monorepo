7734c2b001d43bc797ef9305bf9f6b759cba863d
stephenwf <stephen.fraser@digirati.com>
2019-02-13 10:18:22 +0000

    build

--- bl-work.sh
+++ bl-work.sh
@@ -5,11 +5,11 @@ cd packages;
     rm -rf ./iiif-av-component
     rm -rf ./manifesto
     rm -rf ./universalviewer
-    git clone --single-branch --branch feature/sf-bl https://github.com/stephenwf/iiif-av-component.git
+    git clone --single-branch --branch feature/sf-bl https://github.com/stephenwf/iiif-av-component.git iiif-av-component
     rm -rf !$/.git
-    git clone --single-branch --branch feature/optional-type https://github.com/stephenwf/manifesto.git
+    git clone --single-branch --branch feature/optional-type https://github.com/stephenwf/manifesto.git manifesto
     rm -rf !$/.git
-    git clone --single-branch --branch --recursive feature/sf-bl https://github.com/stephenwf/universalviewer.git
+    git clone --single-branch --branch --recursive feature/sf-bl https://github.com/stephenwf/universalviewer.git universalviewer
     rm -rf !$/.git
 cd -;
 
